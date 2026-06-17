5963969c25d883658897ea45308029a765a138c0
krishnaveniabburi-arch <krishnaveni.abburi@gmail.com>
2026-06-16 23:49:48 -0400

    new logic

--- CRUD-example/index.js
+++ CRUD-example/index.js
@@ -16,10 +16,7 @@ app.use(express.urlencoded({extended: true}))
 app.use((req, res, next) => {
   const time = new Date();
 
-  console.log(
-    `-----
-${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`
-  );
+  console.log(`-----\n${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`);
   if (Object.keys(req.body).length > 0) {
     console.log("Containing the data:");
     console.log(`${JSON.stringify(req.body)}`);
