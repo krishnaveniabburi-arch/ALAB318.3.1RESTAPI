e84457af16a20ab90d1e58f8f25776d5b49de102
krishnaveniabburi-arch <krishnaveni.abburi@gmail.com>
2026-06-16 23:48:06 -0400

    new logic

--- CRUD-example/routes/comments.js
+++ CRUD-example/routes/comments.js
@@ -1,4 +1,14 @@
 import express from "express"
 const router = express.Router();
 
-import users from
+import users from "..//data/users.js";
+import error from "../utilities/error.js";
+import comments from "./routes/comments.js"
+import posts from "../data/posts.js";
+
+const comments = [{}];
+router.route("/").get((req, res) => {
+    res.json(comments);
+});
+
+export default router;
